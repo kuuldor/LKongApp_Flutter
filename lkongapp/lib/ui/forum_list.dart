1496add644f79400f0edeeaa3f3eacc8cb936095
Chengdong Lu <kuuldor@ymail.com>
2019-02-17 02:10:20 -0800

    Fix forum list to load info of forum necessary only.

--- lkongapp/lib/ui/forum_list.dart
+++ lkongapp/lib/ui/forum_list.dart
@@ -23,10 +23,33 @@ import 'package:lkongapp/actions/actions.dart';
 
 import 'package:lkongapp/ui/connected_widget.dart';
 
-class ForumList extends StatelessWidget {
+class ForumList extends StatefulWidget {
+  @override
+  ForumListState createState() {
+    return ForumListState();
+  }
+}
+
+class ForumListState extends State<ForumList> {
+  bool loading;
+
+  void setLoading(bool value) {
+    setState(() {
+      loading = value;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    loading = false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return buildConnectedWidget(context, ForumListModel.fromStore, (viewModel) {
+    return buildConnectedWidget(context, ForumListModel.fromStoreAndState(this),
+        (viewModel) {
       return viewModel.buildListView(context);
     });
   }
@@ -34,7 +57,7 @@ class ForumList extends StatelessWidget {
 
 class ForumListModel extends FetchedListModel {
   final ForumLists repo;
-
+  final ForumListState state;
   final Map<int, Forum> forums;
 
   final List<int> followed;
@@ -66,6 +89,7 @@ class ForumListModel extends FetchedListModel {
 
   ForumListModel({
     @required this.repo,
+    @required this.state,
     @required this.forums,
     @required this.followed,
     @required this.forumIds,
@@ -85,7 +109,7 @@ class ForumListModel extends FetchedListModel {
   @override
   APIRequest get refreshRequest => ForumListRequest(null);
 
-  void _handleLoadInfo(BuildContext context, [int retries = 0]) async {
+  void _handleLoadInfo(BuildContext context, {bool force: false}) async {
     final list = Set<int>();
     if (repo.forums != null && repo.forums.length > 0) {
       list.addAll(repo.forums.map((f) => f.fid));
@@ -94,13 +118,30 @@ class ForumListModel extends FetchedListModel {
       list.addAll(repo.sysplanes.map((f) => f.fid));
     }
 
-    final fetchList = followed.where((fid) => !list.contains(fid)).toSet();
+    var fetchList = followed.where((fid) => !list.contains(fid)).toSet();
 
-    if (showInfo) {
+    if (showInfo || force) {
       fetchList.addAll(list);
     }
-    fetchList
-        .forEach((fid) => dispatchAction(context)(ForumInfoRequest(null, fid)));
+
+    if (!force) {
+      fetchList =
+          fetchList.where((fid) => !repo.info.keys.contains(fid)).toSet();
+    }
+
+    if (fetchList.length > 0) {
+      state.loading = true;
+      final completers = <Future>[];
+      fetchList.forEach((fid) {
+        final completer = Completer<String>();
+        dispatchAction(context)(ForumInfoRequest(completer, fid));
+        completers.add(completer.future);
+      });
+
+      Future.wait(completers).then((values) {
+        state.setLoading(false);
+      });
+    }
   }
 
   @override
@@ -109,56 +150,55 @@ class ForumListModel extends FetchedListModel {
     dispatchAction(context)(ForumListRequest(completer));
     return completer.future.then((error) {
       if (error == null) {
-        _handleLoadInfo(context);
+        _handleLoadInfo(context, force: true);
       }
     });
   }
 
-  static ForumListModel fromStore(Store<AppState> store) {
-    final userData = selectUserData(store);
-    final repo = store.state.uiState.content.forumInfo;
-    final followed = userData?.followList?.fid
-        ?.where((f) => f.length > 0)
-        ?.map((f) => int.parse(f))
-        ?.toList();
+  static fromStoreAndState(ForumListState state) => (Store<AppState> store) {
+        final userData = selectUserData(store);
+        final repo = store.state.uiState.content.forumInfo;
+        final followed = userData?.followList?.fid
+            ?.where((f) => f.length > 0)
+            ?.map((f) => int.parse(f))
+            ?.toList();
 
-    var forumMap = Map<int, Forum>();
-    List<int> forumIds;
-    List<int> planeIds;
+        var forumMap = Map<int, Forum>();
+        List<int> forumIds;
+        List<int> planeIds;
 
-    if (repo.forums != null) {
-      forumMap.addAll(Map.fromIterable(repo.forums, key: (f) => f.fid));
-      forumIds = repo.forums
-          .map((f) => f.fid)
-          .where((fid) => followed?.contains(fid) != true)
-          .toList();
-    }
+        if (repo.forums != null) {
+          forumMap.addAll(Map.fromIterable(repo.forums, key: (f) => f.fid));
+          forumIds = repo.forums
+              .map((f) => f.fid)
+              .where((fid) => followed?.contains(fid) != true)
+              .toList();
+        }
 
-    if (repo.sysplanes != null) {
-      forumMap.addAll(Map.fromIterable(repo.sysplanes, key: (f) => f.fid));
-      planeIds = repo.sysplanes
-          .map((f) => f.fid)
-          .where((fid) => followed?.contains(fid) != true)
-          .toList();
-    }
+        if (repo.sysplanes != null) {
+          forumMap.addAll(Map.fromIterable(repo.sysplanes, key: (f) => f.fid));
+          planeIds = repo.sysplanes
+              .map((f) => f.fid)
+              .where((fid) => followed?.contains(fid) != true)
+              .toList();
+        }
 
-    return ForumListModel(
-      repo: repo,
-      followed: followed,
-      forums: forumMap,
-      forumIds: forumIds,
-      planeIds: planeIds,
-      showInfo: selectSetting(store).showForumInfo,
-    );
-  }
+        return ForumListModel(
+          repo: repo,
+          state: state,
+          followed: followed,
+          forums: forumMap,
+          forumIds: forumIds,
+          planeIds: planeIds,
+          showInfo: selectSetting(store).showForumInfo,
+        );
+      };
 
   @override
   void listIsReady(BuildContext context) {
-    // if (showInfo && repo.info.length == 0) {
-    if (!loading) {
+    if (!loading && !state.loading) {
       _handleLoadInfo(context);
     }
-    // }
   }
 
   Widget createForumListItem(BuildContext context, Forum forum) {
